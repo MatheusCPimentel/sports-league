95792f7b609d65b87b4e0790cb320d75c89fc0d6
Matheus Pimentel <matheus08.pimentel@gmail.com>
2025-10-04 17:57:24 -0300

    chore: prepares SportCard to receive onClick fn

--- src/app/components/SportCard.tsx
+++ src/app/components/SportCard.tsx
@@ -2,25 +2,36 @@
 
 import { League } from "@/types/leagues";
 
-export const SportCard = (league: League) => {
+type SportCardProps = League & {
+  onClick: (leagueId: string) => void;
+};
+
+export const SportCard = ({
+  idLeague,
+  strLeague,
+  strSport,
+  strLeagueAlternate,
+  onClick,
+}: SportCardProps) => {
   return (
-    <div className="bg-white rounded-xl shadow-lg hover:shadow-xl p-6 border-2 border-transparent transition-all duration-300 hover:border-blue-100 cursor-pointer">
+    <div
+      onClick={() => onClick(idLeague)}
+      className="bg-white rounded-xl shadow-lg hover:shadow-xl p-6 border-2 border-transparent transition-all duration-300 hover:border-blue-100 cursor-pointer"
+    >
       <div className="flex flex-col space-y-4">
         <div>
-          <h2 className="text-2xl font-bold text-gray-900 mb-1">
-            {league.strLeague}
-          </h2>
+          <h2 className="text-2xl font-bold text-gray-900 mb-1">{strLeague}</h2>
 
-          {league.strLeagueAlternate && (
+          {strLeagueAlternate && (
             <p className="text-sm text-gray-500 italic">
-              Also known as: {league.strLeagueAlternate}
+              Also known as: {strLeagueAlternate}
             </p>
           )}
         </div>
 
         <div className="flex items-center space-x-2">
           <span className="inline-flex items-center px-3 py-1 rounded-full text-sm font-medium bg-blue-100 text-blue-800">
-            {league.strSport}
+            {strSport}
           </span>
         </div>
       </div>
